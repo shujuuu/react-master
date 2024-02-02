0fff73fb16212acf64504cbb758db8c86ad18c23
shujulin <shuj_lin@yahoo.com>
2024-02-02 18:12:27 -0500

    first commit

---
+++ src/modules/submission/selectors.js
@@ -0,0 +1,4 @@
+import { selectRoot } from '../root';
+
+export const selectSubmission = (name, state) =>
+	selectRoot(name, state).submission;
---
+++ src/modules/submissions/selectors.js
@@ -0,0 +1,4 @@
+import { selectRoot } from '../root';
+
+export const selectSubmissions = (name, state) =>
+	selectRoot(name, state).submissions;
